8cc7a35efc1923ac58fa24ea0800cf3b88b482e9
Taran Pal Singh <taranpal.singh23@gmail.com>
2019-02-15 21:35:03 +0530

    network lag removed

--- hrms_skillset/public/js/user.js
+++ hrms_skillset/public/js/user.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
+    // skill is for user's skills
+    // all_skill is for all skills
+    // and skill database
+    // store all skills in skills array
     $('.js-example-basic-multiple').select2();
+
+
+    all_skills = ["HTML","b","HTML","b"];  ///////////////////////////////
+    var Skills_option = document.getElementById("Skills_option");
+    for (var i = 0; i < all_skills.length; i++) {
+        var option = document.createElement("option");      // TABLE HEADER.
+        option.innerHTML = all_skills[i];
+        option.setAttribute("value","AY");
+        Skills_option.appendChild(option);
+    }
+
+
+
+    skills = ["HTML","b","HTML","b"];  ///////////////////////////////
+    var display_skills = document.getElementById("display_skills");
+    for (var i = 0; i < skills.length; i++) {
+        var skilldiv = document.createElement("div");      // TABLE HEADER.
+        skilldiv.innerHTML = skills[i];
+        skilldiv.setAttribute("class","skill");
+        display_skills.appendChild(skilldiv);
+    }
+
 });
