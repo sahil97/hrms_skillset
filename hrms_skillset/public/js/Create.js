5d6600e5cdbec7de76f0750ecfd8826e6df97904
Taran Pal Singh <taranpal.singh23@gmail.com>
2019-02-16 00:45:48 +0530

    Admin page table added

--- hrms_skillset/public/js/Create.js
+++ hrms_skillset/public/js/Create.js
@@ -1,17 +1,30 @@
 function submitbtn(){
+
   console.log("hello");
   email = document.getElementById("email").value;
   password = document.getElementById("password").value;
+
+  var re = /[a-z]+\.[a-z]+$/;
+  var emailFormat = re.test($("#email").val()); // this returns result in boolean type
+  console.log(emailFormat);
+  if (!emailFormat) {
+    alert("Email id is incorrect");
+  }
+  else if (!password) {
+    alert("Please enter the password");
+  }
+
+
   var myOBJ = {email, password};
   console.log(myOBJ);
 
-  $.ajax({
-      type: 'POST',
-      url: "http://localhost:50434/api/employees/",
-      dataType: "JSON",
-      data :myOBJ,
-      success: function(res){
-        console.log("Created");
-     }
-  });
+  // $.ajax({
+  //     type: 'POST',
+  //     url: "http://localhost:50434/api/employees/",
+  //     dataType: "JSON",
+  //     data :myOBJ,
+  //     success: function(res){
+  //       console.log("Created");
+  //    }
+  // });
 }
--- hrms_skillset/views/Admin.js
+++ hrms_skillset/views/Admin.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
 
     $("#pop").hide();
-    $("#pop").click(function(){
+    $("#list_button").click(function(){
 
-      $.ajax({
-
-          type: 'GET',
-          url: "http://localhost:8083/api/users",
-          dataType: "JSON",
-          success: function(res){
+          $("#pop").show();
+      // $.ajax({
+      //
+      //     type: 'GET',
+      //     url: "http://localhost:8083/api/users",
+      //     dataType: "JSON",
+      //     success: function(res){
 
               var body = document.getElementById('pop');
-              showtable(res);
-
+              jsonObj = [{"Name": "taran", "Id": "33"}];
+              showtable(jsonObj);
+              // showtable(res);
               function showtable(jsonObj) {
 
                   var col = [];
@@ -29,26 +31,29 @@ $(document).ready(function(){
 
 
                   var table = document.createElement("table");
-                  var tr = table.insertRow(-1);                   // TABLE ROW.
+                  var tr = table.insertRow(-1);
 
                   for (var i = 0; i < col.length; i++) {
-                      var th = document.createElement("th");      // TABLE HEADER.
+                      var th = document.createElement("th");  // headings
+                      console.log("headings");
                       th.innerHTML = col[i];
                       tr.appendChild(th);
                   }
+                  table.appendChild(tr);
+
                   for (var i = 0; i < jsonObj.length; i++) {
 
                       tr = table.insertRow(-1);
 
                       for (var j = 0; j < (col.length - 1) ; j++) {
-                          var tabCell = tr.insertCell(-1);
+                          var tabCell = tr.insertCell(-1);                    // values in table cells
                           tabCell.innerHTML = jsonObj[i][col[j]];
                       }
 
                       var tabCell = tr.insertCell(-1);
 
                       var myImage1 = new Image(20, );
-                      myImage1.src = 'edit.png';
+                      myImage1.src = 'edit.png';                      // Edit and Delete images
                       var myImage2 = new Image(20, );
                       myImage2.src = 'delete.png';
                       myImage1.setAttribute("id", jsonObj[i].id);
@@ -59,14 +64,12 @@ $(document).ready(function(){
                       tabCell.appendChild(myImage1);
                       tabCell.appendChild(myImage2);
                       tr.appendChild(tabCell);
+                      table.appendChild(tr);
                   }
                   var pop = document.getElementById("pop");
                   pop.appendChild(table);
-              }
-
 
               }
 
-          });
         });
       });
