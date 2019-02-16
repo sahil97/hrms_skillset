d04a08c649737c2016c39382a0ac432d74b9f4a6
Sahil97 <sahil.sharma071997@gmail.com>
2019-02-16 17:46:57 +0530

    modified users.js to send required data

--- hrms_skillset/routes/users.js
+++ hrms_skillset/routes/users.js
@@ -108,7 +108,8 @@ router.get('/skills',(req,res)=>{
   var token = jwt.verify(req.get('x-auth-token'),'bootcamp');
   console.log(token);
   User.findOne({username:token.username},(err,user)=>{
-    res.json(user.skills);
+    var res_data = {skills:user.skills,name:user.name,username:user.username};
+    res.json(res_data);
   });
 });
 
