2a0dffd3daa3e9ff6a0386d0d54a46796f242f3b
Sahil97 <sahil.sharma071997@gmail.com>
2019-02-16 13:32:28 +0530

    added mongoose conn to user.js

--- hrms_skillset/routes/users.js
+++ hrms_skillset/routes/users.js
@@ -1,36 +1,61 @@
 const express = require('express');
 const router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://kaybee:kaybee7697@ds157641.mlab.com:57641/skillset');
 
-
-
-
-var users = [
-  {id:1,name:"admin",role:"sa",username:"abc@abc.com",password:"test",skills:[],pic:"path.."},
-  {id:2,name:"user",role:"u",username:"u@abc.com",password:"test",skills:["CSS","ES"],pic:"path.."},
-  {id:3,name:"user1",role:"u",username:"a@abc.com",password:"test",skills:[],pic:"path.."}
-]
-
-
-router.get('/',(req,res)=>{
-  res.json(users);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // // we're connected!
+  // console.log("connected");
 });
 
+var userSchema = new mongoose.Schema({
+  name: String,
+  role:String,
+  username:String,
+  password:String,
+  skills:[String],
+  path:String
+});
 
+var User = mongoose.model('User', userSchema);
+
+router.get('/',(req,res)=>{
+  User.find(function (err, users) {
+    if (err) return console.error(err);
+    console.log(users);
+    res.json(users);
+  });
+});
 
 
 /// ---- Login request  -----
 router.post('/login',(req,res)=>{
-  const user = users.find(u => u.username === req.body.username);
-  if(!user) return res.status(404).send("incorrect Username");
+
+  User.findOne({username:req.body.username},(err,user)=>{
+    console.log(user);
+    if(!user) {return res.status(404).send("Incorrect username");}
+
+    else if(user.password == req.body.password){
+        return res.status(200).send("successful login");
+      }
+      else{
+        return res.status(400).send("incorrect password");
+      }
+  });
+
+  // const user = users.find(u => u.username === req.body.username);
+  // if(!user) return res.status(404).send("incorrect Username");
 
   // verifying password
   // console.log(req.body,user);
-  if(user.password == req.body.password){
-    return res.status(200).send("successful login");
-  }
-  else{
-    return res.status(400).send("incorrect password");
-  }
+  // if(user.password == req.body.password){
+  //   return res.status(200).send("successful login");
+  // }
+  // else{
+  //   return res.status(400).send("incorrect password");
+  // }
 });
 
 
@@ -39,55 +64,70 @@ router.post('/login',(req,res)=>{
 
 
 router.post('/register',(req,res)=>{
-  var user = users.find(u => u.username === req.body.username);
-  if(user) return res.status(400).send("User already exists");
+  var user = User.findOne({"username":req.body.username},(err,user)=>{
+    console.log(user);
+    if(user) return res.status(400).send("User already exists");
 
-  user = {id:users.length+1,name:"",role:"u",username:req.body.username,password:req.body.password,skills:[],pic:""};
-  users.push(user);
-  res.json(user);
+    user = new User ({name:"Test",role:"u",username:req.body.username,password:req.body.password,skills:[],pic:""});
+    user.save((err,user)=>{
+      if (err) return console.error(err);
+      console.log("saved");
+    });
+    res.json(user);
+  });
+
 
 });
 
 /// ------ user details change  -----
 router.put('/register/',(req,res)=>{
-  var user = users.find(u => u.username === req.body.uid); // uid == previous username
-  if(!user) return res.status(404).send("incorrect Username");
+    User.findOneAndUpdate({"username":req.body.uid}, {username:req.body.username,password:req.body.password},{new: true}, (err,user)=>{
+      if( !user) {
+                // console.log(err);
+                // console.error( JSON.stringify( err ) );
+                return res.status(404).send( "Username not found" );
+            }
 
-  user.username = req.body.username;
-  user.password = req.body.password;
-
-  res.json(user);
+        console.log("saved");
+        res.json(user);
+    });
 });
 
 
 //// ----- user delete -----
 
 router.delete('/register',(req,res)=>{
-  var user = users.find(u => u.username === req.body.username); // uid == previous username
-  if(!user) return res.status(404).send("incorrect Username");
 
-  users.splice(skills.indexOf(user),1);
-  res.send(user);
+  User.findOneAndRemove({"username":req.body.username}, (err,user)=>{
+    if( !user ) {
+              // console.error( JSON.stringify( error ) );
+              return res.status(404).send( "username not found" );
+          }
 
+      console.log("saved");
+      res.json(user);
+  });
 });
 
 /// --- get user skills ------
 router.get('/skills',(req,res)=>{
-  var user = users.find(u => u.username === req.body.username); // uid == previous username
-  if(!user) return res.status(404).send("incorrect Username");
 
-  res.send(user.skills);
+  User.findOne({username:req.body.username},(err,user)=>{
+    res.json(user.skills);
+  });
 });
 
 
 
 /// --- Edit user Skills ---
 router.put('/skills',(req,res)=>{
-  var user = users.find(u => u.username === req.body.username); // uid == previous username
-  if(!user) return res.status(404).send("incorrect Username");
 
-  user.skills = user.skills.concat(req.body.skills);
-  res.send(user.skills);
+  User.findOne({username:req.body.username},(err,user)=>{
+    user.skills = user.skills.concat(req.body.skills);
+    user.save((err,user)=>{
+      res.json(user);
+    });
+  });
 });
 
 
