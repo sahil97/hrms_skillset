f4a52d055d4f75a84afbf2548214fbef84199057
Sahil97 <sahil.sharma071997@gmail.com>
2019-02-25 17:32:28 +0530

    changing index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 // app.use(express.static(__dirname + '/public'));
 
 // if(env == "development"){
