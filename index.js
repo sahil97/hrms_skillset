cae8b0e97846659519ee8a4b373976da93a17803
Sahil97 <sahil.sharma071997@gmail.com>
2019-02-25 17:28:03 +0530

    changing index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
 const config = require('config');
 const user = require('./routes/users.js')
